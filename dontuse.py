5c48e86b294596f8216236c622dbe5ec6838375f
aakashvarma <aakashvarma18@gmail.com>
2019-05-07 14:44:24 +0530

    tested the stackoverflow API

--- dontuse.py
+++ dontuse.py
@@ -1,26 +1,33 @@
-from subprocess import Popen, PIPE
+# from subprocess import Popen, PIPE
 
-def execute():
-    process = Popen(
-            ["python", "testprocesss.py"],
-            cwd=None,
-            shell=False,
-            close_fds=True,
-            stdout=PIPE,
-            stderr=PIPE,
-            bufsize=1
-        )
+# def execute():
+#     process = Popen(
+#             ["python", "testprocesss.py"],
+#             cwd=None,
+#             shell=False,
+#             close_fds=True,
+#             stdout=PIPE,
+#             stderr=PIPE,
+#             bufsize=1
+#         )
     
-    errors = []
-    # read error lines from the PIPE
-    for line in process.stderr.readline():
-        errors.append(line)
+#     errors = []
+#     # read error lines from the PIPE
+#     for line in process.stderr.readline():
+#         errors.append(line)
     
-    out = ' '.join(errors)
+#     out = ' '.join(errors)
 
-    print out
+#     print out
 
-    # print process.stdout.readline()
-    # print process.stderr.readline().encode('UTF-8')
+#     # print process.stdout.readline()
+#     # print process.stderr.readline().encode('UTF-8')
 
-execute()
+# execute()
+
+import requests
+import json 
+
+r = requests.get('https://api.stackexchange.com/2.2/search?order=desc&sort=relevance&tagged=python&intitle=ZeroDivisionError%3A%20integer%20division%20or%20modulo%20by%20zero&site=stackoverflow')
+
+print(json.dumps(r.json(), indent=4, sort_keys=True))
