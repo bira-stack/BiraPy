6088ca098c1a57248ce7410bb1aa7c808fa27bb6
aakashvarma <aakashvarma18@gmail.com>
2019-05-07 21:53:59 +0530

    get_url method to return the API url for respective error message

--- dontuse.py
+++ dontuse.py
@@ -27,7 +27,41 @@
 
 import requests
 import json 
+from requests.utils import quote
+try:
+    import urlparse
+    from urllib import urlencode
+except: # For Python 3
+    import urllib.parse as urlparse
+    from urllib.parse import urlencode
 
-r = requests.get('https://api.stackexchange.com/2.2/search?order=desc&sort=relevance&tagged=python&intitle=ZeroDivisionError%3A%20integer%20division%20or%20modulo%20by%20zero&site=stackoverflow')
+# ZeroDivisionError%3A%20integer%20division%20or%20modulo%20by%20zero  --> intitle format
+
+def get_url():
+    url = 'https://api.stackexchange.com/2.2/search?'
+    params = {
+        'order':'desc', 
+        'sort':'relevance', 
+        'tagged':'python', 
+        'intitle': 'ZeroDivisionError: integer division or modulo by zero',
+        'site': 'stackoverflow'
+        }
+
+    url_parts = list(urlparse.urlparse(url))
+    query = dict(urlparse.parse_qsl(url_parts[4]))
+    query.update(params)
+    url_parts[4] = urlencode(query)
+    url = urlparse.urlunparse(url_parts).strip()
+
+    return url
+
+
+
+
+r = requests.get(get_url())
 
 print(json.dumps(r.json(), indent=4, sort_keys=True))
+
+######
+
+
